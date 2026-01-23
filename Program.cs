c227b2468b19c1ac02914ffa43cc8f173b53fcc3
LuissMello <lhnmello@hotmail.com>
2026-01-23 10:19:00 -0300

    removendo totalmente opentelemetry

--- Program.cs
+++ Program.cs
@@ -10,10 +10,6 @@ using Microsoft.Extensions.Options;
 using System.Globalization;
 using System.Net;
 
-// ===== Telemetria (Application Insights via OpenTelemetry - Azure Monitor) =====
-using Azure.Monitor.OpenTelemetry.AspNetCore;
-using OpenTelemetry.Resources;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseUrls("http://0.0.0.0:8080");
